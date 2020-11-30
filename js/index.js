e0ad4f4cd064eaccb84e643d0047b607be7c3cbe
wonhee-hong <71586923+wonhee-hong@users.noreply.github.com>
2020-11-30 14:43:38 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$( ".l1" ).on('click',function() {
+         if ($(".leg2").css("display") == "none") {
+          $( ".lego2" ).css("display","block");
+      } 
+ })
+})
+
+
